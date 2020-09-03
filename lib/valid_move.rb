434001b150b89f8fb4eda000e23baf19c21c9bdf
Devin Cooper <dcoope12@asu.edu>
2020-09-03 21:08:19 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -17,10 +17,8 @@ else
 end
 end
 
-def position_taken?(board, index)
-if board[index] == " " || board[index] == "" || board[index] == nil
-return false
-elsif board[index] == "X" || board[index] == "O"
-return true
-end
+if  (position_taken?(board,index) == false && (on_board?(index) == true)
+  return true
+else
+  return false
 end
