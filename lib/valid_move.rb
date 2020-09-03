0161d85c8f75a7500cda59d91376e82c9c71c803
Devin Cooper <dcoope12@asu.edu>
2020-09-03 20:53:28 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,10 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
-  	if position_taken? == false
-   return false
-elsif position_taken? == true
-  return true
+  	if position_taken? 
+      return true
+    else 
+      return false
+    
   end
 end
 
